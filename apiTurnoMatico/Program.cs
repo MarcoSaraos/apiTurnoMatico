43c6b599146bac30ec257c0fd59645a527b30bce
jazr <licjazepeda@gmail.com>
2025-03-28 11:32:52 -0600

    added swagger

--- apiTurnoMatico/Program.cs
+++ apiTurnoMatico/Program.cs
@@ -6,11 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+#region [ Configuracion de Swagger ]
+builder.Services.AddSwaggerGen();
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    #region [ Configuracion de Swagger ]
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    #endregion
     app.MapOpenApi();
 }
 
